cebb6af9ab669c307fbb17757c138a215739cf5f
souryu224 <68739177+souryu224@users.noreply.github.com>
2020-08-05 16:00:07 +0900

    + Calc1.java

--- Calc1.java
+++ Calc1.java
@@ -5,5 +5,10 @@ public class Calc1{
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         String str = br.readLine();
         System.out.println("str = \"" + str + "\"");
+        System.out.println("str = \"" + str + "\"");
+        System.out.println("str = \"" + str + "\"");
+        System.out.println("str = \"" + str + "\"");
+        System.out.println("str = \"" + str + "\"");
+        System.out.println("str = \"" + str + "\"");
     }
 }
