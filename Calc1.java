b8d12d624ac4f442606885cf54449097e8aa2a39
souryu224 <68739177+souryu224@users.noreply.github.com>
2020-08-05 15:46:31 +0900

    + Calc1.java

---
+++ Calc1.java
@@ -0,0 +1,9 @@
+import java.io.*;
+
+public class Calc1{
+    public static void main(String[] args)throws IOException{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        String str = br.readLine();
+        System.out.println("str = \"" + str + "\"");
+    }
+}
