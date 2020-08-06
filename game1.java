e5fd4e87899bb32b44115437e89cbdc6846e0309
souryu224 <68739177+souryu224@users.noreply.github.com>
2020-08-06 22:37:57 +0900

    RandomNumber

---
+++ game1.java
@@ -0,0 +1,31 @@
+import java.io.*;
+
+import javax.imageio.stream.ImageInputStream;
+
+public class game1 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int hitNumber;
+        int n = -1 ;
+        int count;
+
+        hitNumber = (int)(Math.random() * 100);
+
+        while( hitNumber != n ){
+
+            System.out.print("0~100 n:");
+            n = Integer.parseInt(br.readLine());
+
+            if(hitNumber < n)
+                System.out.println("hitNumber < n");
+            
+            else if(hitNumber > n)
+                System.out.println("hitNumber > n");
+            
+            else System.out.println("hitNumber == n");
+
+        }
+        System.out.println("-----------------");
+    }
+}
